f5c3a68c6e3e48fbf43b4201888fbdcde70d5dca
Letmeouted <1002726239@qq.com>
2021-06-09 08:16:39 +0000

    change zip method

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 const http = require("http");
-
+const JSZIP = require("jszip");
 var cwd = process.cwd();
 const compressing = require('compressing');
 console.log(cwd)
@@ -119,35 +119,67 @@ http.createServer(function (req, res) {
         _readStream.pipe(res);
       } else {
         console.log('是否是文件夹：' + stat.isDirectory())
-        const { spawn } = require('child_process');
-        const zip = spawn('zip', ['-r', fileName + '.zip', fpath]);
+        const filezip = fileName + '.zip'
+        console.log(filezip)
+        console.log(__dirname)
+        let path = require("path");//工具模块，处理文件路径的小工具
+        let zip = new JSZIP();
 
-        zip.stdout.on('data', (data) => {
-          console.log(`标准输出: ${data}`);
-        });
+        //读取目录及文件
+        function readDir(obj, nowPath) {
+          let files = fs.readdirSync(nowPath);//读取目录中的所有文件及文件夹（同步操作）
+          files.forEach(function (fileName, index) {//遍历检测目录中的文件
+            console.log(fileName, index);//打印当前读取的文件名
+            let fillPath = nowPath + "/" + fileName;
+            let file = fs.statSync(fillPath);//获取一个文件的属性
+            if (file.isDirectory()) {//如果是目录的话，继续查询
+              let dirlist = zip.folder(fileName);//压缩对象中生成该目录
+              readDir(dirlist, fillPath);//重新检索目录文件
+            } else {
+              obj.file(fileName, fs.readFileSync(fillPath));//压缩目录添加文件
+            }
+          });
+        }
 
-        zip.stderr.on('data', (data) => {
-          console.error(`标准错误: ${data}`);
-        });
+        //开始压缩文件
+        function startZIP() {
+          var currPath = __dirname;//文件的绝对路径 当前当前js所在的绝对路径
+          var targetDir = path.join(currPath, "files/" + fileName);
+          readDir(zip, targetDir);
+          zip.generateAsync({//设置压缩格式，开始打包
+            type: "nodebuffer",//nodejs用
+            compression: "DEFLATE",//压缩算法
+            compressionOptions: {//压缩级别
+              level: 9
+            }
+          }).then(function (content) {
+            fs.writeFileSync(currPath + '/zip/'+fileName + ".zip", content, "utf-8");//将打包的内容写入 当前目录下的 .zip中
+            res.setHeader(
+              "Content-disposition",
+              "attachment; filename=" + filezip
+            );
+            res.writeHead(200, { "Content-Type": "application/octet-stream" });
+            const _readStream = fs.createReadStream('./zip/'+filezip);
+            _readStream.pipe(res);
+          });
+        }
+        
+        startZIP();
+        // const { spawn } = require('child_process');
 
-        zip.on('close', (code) => {
-          console.log(`子进程使用代码 ${code} 退出`);
-        });
-        // const { execFile } = require('child_process');
-        // const child = execFile('zip', ['-r', fileName + '.zip', fpath], (error, stdout, stderr) => {
-        //   if (error) {
-        //     throw error;
-        //   }
-        //   console.log(stdout);
+        // const zip = spawn('zip', ['-r', filezip, fpath]);
+
+        // zip.stdout.on('data', (data) => {
+        //   console.log(`标准输出: ${data}`);
         // });
-        // console.log(child)
-        res.setHeader(
-          "Content-disposition",
-          "attachment; filename=" + fileName + '.zip'
-        );
-        res.writeHead(200, { "Content-Type": "application/octet-stream" });
-        const _readStream = fs.createReadStream('./' + fileName + '.zip');
-        _readStream.pipe(res);
+
+        // zip.stderr.on('data', (data) => {
+        //   console.error(`标准错误: ${data}`);
+        // });
+
+        // zip.on('close', (code) => {
+        //   console.log(`子进程使用代码 ${code} 退出`);
+        // });
       }
     } catch (err) {
       res.writeHead(400);
@@ -155,7 +187,6 @@ http.createServer(function (req, res) {
     }
     return;
   }
-  // spawn
   if (_lower_url.startsWith("/download")) {
     res.writeHead(200, { "Content-Type": "text/html" });
     const data = fs.readFileSync("./downloads.html");
