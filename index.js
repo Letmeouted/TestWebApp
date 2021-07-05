c8c65e36e9ac77e2f4edf2b9f1b993b87f698417
Letmeouted <1002726239@qq.com>
2021-07-05 16:03:05 +0800

    delete extra annotation line

--- index.js
+++ index.js
@@ -308,8 +308,6 @@ http.createServer(function (req, res) {
     // });
     // res.writeHead(200, { "Content-Type": "text/html" })
     // return;
-    // hyu7112810001
-    // JZyy123456
   }
   res.writeHead(404, { "Content-Type": "text/html" });
   res.end(
