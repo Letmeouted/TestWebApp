a6853b8d9bed09db8bb98ff5b67cc67e8392e88e
Letmeouted <1002726239@qq.com>
2021-07-05 15:38:05 +0800

    delete extra line

--- index.js
+++ index.js
@@ -300,11 +300,9 @@ http.createServer(function (req, res) {
     // bat.stdout.on('data', (data) => {
     //   console.log(data.toString());
     // });
-
     // bat.stderr.on('data', (data) => {
     //   console.error(data.toString());
     // });
-
     // bat.on('exit', (code) => {
     //   console.log(`Child exited with code ${code}`);
     // });
